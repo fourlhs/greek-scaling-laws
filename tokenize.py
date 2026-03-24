59a2bbb0118dcec51ffd8d055183e255289de69c
Nikos Fourlis <el24189@mail.ntua.gr>
2026-03-24 14:29:18 +0200

    feat: add tokenize.py and gitignore

---
+++ tokenize.py
@@ -0,0 +1,28 @@
+from datasets import load_dataset
+from tokenizers import Tokenizer
+from tokenizers.models import BPE
+from tokenizers.trainers import BpeTrainer
+from tokenizers.pre_tokenizers import Whitespace # what splits text before BPE merges?
+from tokenizers.normalizers import Sequence, NFD, Lowercase
+
+def get_training_corpus(dataset):
+    # yield batches of strings from greek_translation column
+    # hint: iterate in chunks of 1000 for memory efficiency
+    batch_size = 1000
+    for i in range(0, len(dataset), batch_size):
+        yield dataset[i : i + 1000]["greek_translation"]
+
+dataset = load_dataset("alexliap/tinystories-gr", split="train")
+
+tokenizer = Tokenizer(BPE(unk_token="<|unk|>"))
+tokenizer.normalizer = Sequence([NFD(), Lowercase()])
+tokenizer.pre_tokenizer = Whitespace()
+
+trainer = BpeTrainer(
+    vocab_size=16000,
+    special_tokens=["<|endoftext|>", "<|unk|>", "<|pad|>"],
+    min_frequency=2
+)
+
+tokenizer.train_from_iterator(get_training_corpus(dataset), trainer=trainer)
+tokenizer.save("greek_bpe_tokenizer.json")
