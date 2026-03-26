d84cb2728066c905aa44ee1e17ea236c90842a44
Nikos Fourlis <el24189@mail.ntua.gr>
2026-03-26 11:25:46 +0200

    retrain tokenizer with ByteLevel, preserve case and whitespace

--- train_tokenizer.py
+++ train_tokenizer.py
@@ -2,8 +2,8 @@ from datasets import load_dataset
 from tokenizers import Tokenizer
 from tokenizers.models import BPE
 from tokenizers.trainers import BpeTrainer
-from tokenizers.pre_tokenizers import Whitespace
-from tokenizers.normalizers import Sequence, NFD, Lowercase
+from tokenizers.pre_tokenizers import ByteLevel
+from tokenizers.decoders import ByteLevel as ByteLevelDecoder
 
 def get_training_corpus(dataset):
     batch_size = 1000
@@ -12,14 +12,14 @@ def get_training_corpus(dataset):
 
 dataset = load_dataset("alexliap/tinystories-gr", split="train")
 
-tokenizer = Tokenizer(BPE(unk_token="<|unk|>"))
-tokenizer.normalizer = Sequence([NFD(), Lowercase()])
-tokenizer.pre_tokenizer = Whitespace()
+tokenizer = Tokenizer(BPE())
+tokenizer.pre_tokenizer = ByteLevel(add_prefix_space=False)
+tokenizer.decoder = ByteLevelDecoder()
 
 trainer = BpeTrainer(
     vocab_size=16000,
-    special_tokens=["<|endoftext|>", "<|unk|>", "<|pad|>"],
-    min_frequency=2
+    special_tokens=["<|endoftext|>", "<|pad|>"],
+    initial_alphabet=ByteLevel.alphabet()
 )
 
 tokenizer.train_from_iterator(get_training_corpus(dataset), trainer=trainer)
